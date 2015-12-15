1aa7d9f689ce665bfd19bf763896dfa24f801736
Samar Panda <er.samarpanda@gmail.com>
2015-12-15 09:03:18 +0530

    Initial form added

--- app/js/main.jsx
+++ app/js/main.jsx
@@ -6,6 +6,12 @@ var SearchQuery = require('./search-query').init;
 
 var Contactsearch = require('./components/contactsearch.jsx').contactsearch;
 
+var CreateContact = require('./components/createcontact.jsx').createcontact;
+
+ReactDOM.render(<CreateContact/>, document.getElementById('create-contact'), () => {
+	console.log('post create contact render');
+});
+
 ReactDOM.render(<Contactsearch />, document.getElementById('search'), () => {
 	SearchQuery();
 });
